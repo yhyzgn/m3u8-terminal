b96acc4330f3031b29984a0a19601e7c24577b27
颜洪毅 <yhyzgn@gmail.com>
2020-12-29 11:51:43 +0800

    update

--- dl/dl_test.go
+++ dl/dl_test.go
@@ -13,33 +13,33 @@ import (
 	"testing"
 )
 
-//func TestNew(t *testing.T) {
-//	// initialize progress container, with custom width
-//	p := mpb.New(mpb.WithWidth(64))
-//
-//	total := 100
-//	name := "Single Bar:"
-//	// adding a single bar, which will inherit container's width
-//	bar := p.AddBar(int64(total),
-//		mpb.PrependDecorators(
-//			// display our name with one space on the right
-//			decor.Name(name, decor.WC{W: len(name) + 1, C: decor.DidentRight}),
-//			// replace ETA decorator with "done" message, OnComplete event
-//			decor.OnComplete(
-//				decor.AverageETA(decor.ET_STYLE_GO, decor.WC{W: 4}), "done",
-//			),
-//		),
-//		mpb.AppendDecorators(decor.Percentage()),
-//	)
-//	// simulating some work
-//	max := 100 * time.Millisecond
-//	for i := 0; i < total; i++ {
-//		time.Sleep(time.Duration(rand.Intn(10)+1) * max / 10)
-//		bar.Increment()
-//	}
-//	// wait for our bar to complete and flush
-//	p.Wait()
-//}
+func TestNew(t *testing.T) {
+	// initialize progress container, with custom width
+	p := mpb.New(mpb.WithWidth(64))
+
+	total := 100
+	name := "Single Bar:"
+	// adding a single bar, which will inherit container's width
+	bar := p.AddBar(int64(total),
+		mpb.PrependDecorators(
+			// display our name with one space on the right
+			decor.Name(name, decor.WC{W: len(name) + 1, C: decor.DidentRight}),
+			// replace ETA decorator with "done" message, OnComplete event
+			decor.OnComplete(
+				decor.AverageETA(decor.ET_STYLE_GO, decor.WC{W: 4}), "done",
+			),
+		),
+		mpb.AppendDecorators(decor.Percentage()),
+	)
+	// simulating some work
+	max := 100 * time.Millisecond
+	for i := 0; i < total; i++ {
+		time.Sleep(time.Duration(rand.Intn(10)+1) * max / 10)
+		bar.Increment()
+	}
+	// wait for our bar to complete and flush
+	p.Wait()
+}
 
 func TestDownloader_Start(t *testing.T) {
 	downloader := New("./tst").ShowProgressBar(false)
